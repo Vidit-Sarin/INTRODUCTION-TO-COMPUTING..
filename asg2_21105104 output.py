57e4a6636906198c111c005105dcf6df19227fe5
Vidit <97087890+Vidit-Sarin@users.noreply.github.com>
2022-01-17 17:32:50 +0530

    Update asg2_21105104 output.py

--- asg2_21105104 output.py
+++ asg2_21105104 output.py
@@ -2,12 +2,41 @@ Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (
 Type "help", "copyright", "credits" or "license()" for more information.
 
 =========== RESTART: C:\Users\vidit\OneDrive\Desktop\asg2_21105104.py ==========
+    
+                                             ##ASSIGNMENT - 2
+
+## 1. For a given input string “Python is a case sensitive language”. Write python
+##    code for the following:
+# a. Find the length of the input string.
+
 The length of string is 35
+
+# b. Reverse the order of the string in one line code.
+
 egaugnal evitisnes esac a si nohtyP
+
+# c. Using Slice function store “a case sensitive” in new string.
+
 New sliced string:-  a case sensitive language
+
+# d. Replace “a case sensitive” with “object oriented”.
+
 New string:-  Python is object oriented language
+  
+# e. Find index of substring “a” in the given input string
+
 Index of required substring is-  10
+
+##   f. Remove the white spaces from the given input string.
 New string:-  Pythonisacasesensitivelanguage
+
+
+## 2. Store your name, SID, department name and CGPA into different variables.
+##    With the help of String formatting print the following output:
+##    Hey, ABC Here!
+##    My SID is 2110XXXX
+##    I am from XYZ department and my CGPA is 9.9
+
 Enter your name here:- Vidit Sarin
 Enter your SID here:- 21105104
 Enter your Department Name here:- Electronics and Communication
@@ -15,19 +44,50 @@ Enter your CGPA here:- 10
 Hey, Vidit Sarin Here!
 My SID is 21105104
 I am from Electronics and Communication department and my CGPA is 10.000000
+
+
+## 3. For a=56 and b=10 with the help of bitwise operators calculate the following:
+
+# a. a&b
+
 a&b=  8
+
+# b. a/b
+
 a/b=  5.6
+
+# c. a^b
+
 a^b=  50
+
+# d. Left shift both a and b with 2 bits.
+
 After left shift by 2-bits a=  224
 After left shift by 2-bits b=  40
+
+# e. Right shift a with 2 bits and b with 4 bits.
+
 After right shift by 2-bits a=  14
 After right shift by 2-bits b=  0
+
+## 4. Write a python program to find the greatest of three numbers entered by the user.
+
 Enter the first number-  30
 Enter the second number-  40
 Enter the thied number-  50
 The greatest number is- 50
+
+## 5. Write a python program to check if the word “name” is present in the string entered by the user (Print : “Yes” or “No”).
+
 Enter the sentence/string - My name is Vidit Sarin
 Yes
+
+## 6. For any three lengths, there is a simple test to see if it is possible to form a
+##    triangle. If any of the three lengths is greater than the sum of the other two,
+##    then you cannot form a triangle. Otherwise, Enter three sides of a triangle,
+##    converts them to integers, and to check whether the given input lengths can
+##    form a triangle or not (Print : “Yes” or “No”).
+
 Enter the first side-  3
 Enter the second side-  4
 Enter the thied side-  5
