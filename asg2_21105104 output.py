a71cf9045e58f865031ad16cee195f1bd6fc9ace
Vidit <97087890+Vidit-Sarin@users.noreply.github.com>
2022-01-17 17:24:12 +0530

    Add files via upload

---
+++ asg2_21105104 output.py
@@ -0,0 +1,34 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=========== RESTART: C:\Users\vidit\OneDrive\Desktop\asg2_21105104.py ==========
+The length of string is 35
+egaugnal evitisnes esac a si nohtyP
+New sliced string:-  a case sensitive language
+New string:-  Python is object oriented language
+Index of required substring is-  10
+New string:-  Pythonisacasesensitivelanguage
+Enter your name here:- Vidit Sarin
+Enter your SID here:- 21105104
+Enter your Department Name here:- Electronics and Communication
+Enter your CGPA here:- 10
+Hey, Vidit Sarin Here!
+My SID is 21105104
+I am from Electronics and Communication department and my CGPA is 10.000000
+a&b=  8
+a/b=  5.6
+a^b=  50
+After left shift by 2-bits a=  224
+After left shift by 2-bits b=  40
+After right shift by 2-bits a=  14
+After right shift by 2-bits b=  0
+Enter the first number-  30
+Enter the second number-  40
+Enter the thied number-  50
+The greatest number is- 50
+Enter the sentence/string - My name is Vidit Sarin
+Yes
+Enter the first side-  3
+Enter the second side-  4
+Enter the thied side-  5
+Yes..the given input lengths can form a triangle
