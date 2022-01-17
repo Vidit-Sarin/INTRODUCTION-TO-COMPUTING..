eb13da3201b6c29e31a63da60ea415cde7a6d17d
Vidit <97087890+Vidit-Sarin@users.noreply.github.com>
2022-01-17 17:44:23 +0530

    Add files via upload

---
+++ asg_21105104 output 3.py
@@ -0,0 +1,25 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======= RESTART: C:/Users/vidit/OneDrive/Desktop/asg_21105104 output 2.py ======
+Enter first number -30
+Enter second number -40
+Enter third number - 50
+40.0
+Enter the Gross Income to the nearest penny -4500000
+Enter the number of Dependents -4
+895600.0
+Enter your SID-21105104
+Enter your name-Vidit Sarin
+Enter your Gender-(M,F,U) -  M
+Enter the course name-ECE
+Enter the CGPA-10
+[21105104, 'Vidit Sarin', 'M', 'ECE', 10.0]
+Enter the marks in Subject 1 = 98
+Enter the marks in Subject 2 = 99
+Enter the marks in Subject 3 = 97
+Enter the marks in Subject 4 = 98
+Enter the marks in Subject 5 = 100
+[97.0, 98.0, 98.0, 99.0, 100.0]
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Purple', 'Yellow']
