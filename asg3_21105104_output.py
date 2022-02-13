a0a0bdbdee2205ab4dbfd14c46b8d81720d4b697
Vidit <97087890+Vidit-Sarin@users.noreply.github.com>
2022-02-13 17:36:46 +0530

    Update asg3_21105104_output.py

--- asg3_21105104_output.py
+++ asg3_21105104_output.py
@@ -2,16 +2,21 @@ Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (
 Type "help", "copyright", "credits" or "license()" for more information.
 
 =============== RESTART: C:/Users/vidit/OneDrive/Desktop/vidit.py ==============
+# Q.1
 Enter the string - Coffee
 {'c': 1, 'o': 1, 'f': 2, 'e': 2}
+# Q.2
 Enter current date =  29
 Enter current month = 2
 Enter current year =  2000
 Next Date is:-  1 / 3 / 2000
+# Q.3
 ENTER THE LIST (seperate the entered numbers by commas)  :  1,2,3,4,5,6,7,8,9
 The output is -  [(1, 1), (2, 4), (3, 9), (4, 16), (5, 25), (6, 36), (7, 49), (8, 64), (9, 81)]
+# Q.4
 Enter the Grade Point - 10
 Your Grade is ‘A+’ and Performance is Outstanding.
+# Q.5
 Enter the integer = 7
 ABCDEFGHIJK
  ABCDEFGHI
@@ -19,7 +24,7 @@ ABCDEFGHIJK
    ABCDE
     ABC
      A
-      
+# Q.6    
 Do you want to enter name and SID of student?
               Press Y for Yes and N for No - Y
 Enter the name of the student - VIDIT SARIN
@@ -38,12 +43,14 @@ Printing the record of student information: -   {21105104: 'VIDIT SARIN', 211050
 Sorting on basis of values ['ADARSH SINGH', 'SARTHAK KALIA', 'VIDIT SARIN']
 Sorting on basis of keys [(21105042, 'SARTHAK KALIA'), (21105089, 'ADARSH SINGH'), (21105104, 'VIDIT SARIN')]
 Enter the SID to search Name:- 21105104
-ADARSH SINGH
+VIDIT SARIN
+# Q.7
 Enter the number - 8
 The required series is - 
 1 1 2 3 5 8 13 21 
 Total sum of the series till the entered term =  54
-The average is =  54
+The average is =  6.75
+# Q.8
 a). Set of all elements that are in Set1 and Set2 but not both -  {1, 3, 5, 6, 8}
 b). Set of all elements that are in only one of the three sets Set1,Set2 and Set3. -  {17, 3, 6, 8, 9, 13}
 c). Set of elements that are exactly two of the sets Set1, Set2 and Set3 -  {1, 2, 4, 5}
