e63a8bc1f77951f3dfea96e8d450120130437c02
Vidit <97087890+Vidit-Sarin@users.noreply.github.com>
2022-02-13 17:32:38 +0530

    Add files via upload

---
+++ asg3_21105104_output.py
@@ -0,0 +1,51 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: C:/Users/vidit/OneDrive/Desktop/vidit.py ==============
+Enter the string - Coffee
+{'c': 1, 'o': 1, 'f': 2, 'e': 2}
+Enter current date =  29
+Enter current month = 2
+Enter current year =  2000
+Next Date is:-  1 / 3 / 2000
+ENTER THE LIST (seperate the entered numbers by commas)  :  1,2,3,4,5,6,7,8,9
+The output is -  [(1, 1), (2, 4), (3, 9), (4, 16), (5, 25), (6, 36), (7, 49), (8, 64), (9, 81)]
+Enter the Grade Point - 10
+Your Grade is ‘A+’ and Performance is Outstanding.
+Enter the integer = 7
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+      
+Do you want to enter name and SID of student?
+              Press Y for Yes and N for No - Y
+Enter the name of the student - VIDIT SARIN
+Enter the SID of the student - 21105104
+Do you want to enter name and SID of student?
+              Press Y for Yes and N for No - Y
+Enter the name of the student - SARTHAK KALIA
+Enter the SID of the student - 21105042
+Do you want to enter name and SID of student?
+              Press Y for Yes and N for No - Y
+Enter the name of the student - ADARSH SINGH
+Enter the SID of the student - 21105089
+Do you want to enter name and SID of student?
+              Press Y for Yes and N for No - N
+Printing the record of student information: -   {21105104: 'VIDIT SARIN', 21105042: 'SARTHAK KALIA', 21105089: 'ADARSH SINGH'}
+Sorting on basis of values ['ADARSH SINGH', 'SARTHAK KALIA', 'VIDIT SARIN']
+Sorting on basis of keys [(21105042, 'SARTHAK KALIA'), (21105089, 'ADARSH SINGH'), (21105104, 'VIDIT SARIN')]
+Enter the SID to search Name:- 21105104
+ADARSH SINGH
+Enter the number - 8
+The required series is - 
+1 1 2 3 5 8 13 21 
+Total sum of the series till the entered term =  54
+The average is =  54
+a). Set of all elements that are in Set1 and Set2 but not both -  {1, 3, 5, 6, 8}
+b). Set of all elements that are in only one of the three sets Set1,Set2 and Set3. -  {17, 3, 6, 8, 9, 13}
+c). Set of elements that are exactly two of the sets Set1, Set2 and Set3 -  {1, 2, 4, 5}
+d). Set of all integers in the range 1 to 10 that are not in Set1 -  {6, 7, 8, 9, 10}
+e). Set of all integers in the range 1 to 10 that are not in Set1,Set2 and Set3 -  {10, 7}
